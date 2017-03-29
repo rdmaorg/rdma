4566b9729df5e356df91ed22a17c7ec3f61412e2
806827 <devnull@localhost>
2017-03-29 11:52:39 +0100

    Server page FrontEnd

--- gdma2-web/src/main/resources/public/main/page/servers.js
+++ gdma2-web/src/main/resources/public/main/page/servers.js
@@ -1,9 +1,11 @@
+var savedServerID = -1;
 var configureDataTable = function(){
 	
 	var config={
 	        fixedHeader: true,
-			order: [[ 1, "asc" ]],
+			order: [[ 0, "asc" ]],
 			"lengthMenu": [ [25, 50, 100], [25, 50, 100] ],
+			"columnDefs": [ { "orderable": false, "targets": 8 } ],
 			"columns": [
 			            { "data": "id" },
 			            { "data": "name" },
@@ -12,23 +14,160 @@ var configureDataTable = function(){
 			            { "data": "connectionUrl" },
 			            { "data": "connectionType.name", "defaultContent": "" },
 			            { "data": "prefix" },
-			            { "data": "active" }
+			            { "data": "active" },
+			            { "data": "active","render" : function(data, type, row){ 	
+			            	return '<button class="btn btn-primary btn-xs editServer" data-serverid="'+ row.id+ '"><span class="glyphicon glyphicon-pencil"></span> Edit</button>'
+			            	+ '&nbsp;'
+			            	+'<button class="btn btn-warning btn-xs deleteServer" data-serverid="'+ row.id+ '"><span class="glyphicon glyphicon-remove"></span> Delete</button>';
+			            	} 
+			            }
 			        ]
 	};
 
-	$('#tbl_server').configureDataTable(config, {url: restUri.server.table, complete: function(){
-//		hideLoading();
-	}});
+	tblServerList = $('#tbl_server').configureDataTable(config, {
+		url: restUri.server.table,
+		complete: function(){
+			//hideLoading();
+			associateDeleteServer();
+			associateEditServer();
+			associatePostServer();
+		},
+		error: function(message, e){
+			console.error("ERROR: " + JSON.stringify(e));
+			handleError('#global-alert',e);
+		}
+	});
+	
+	tblServerList.off( 'responsive-display');
+	tblServerList.on( 'responsive-display', function ( e, datatable, row) {
+	    associateDeleteServer();
+		associateEditServer();
+	} );
 };
 
+//Edit server data
+var associateEditServer = function(){
+	$('.editServer').click(function(){
+ 		var btn = $(this);
+//		var btn = element;
+//		console.log("Deleting Server Id " + btn.data('serverid'));
+		editServer(btn.data('serverid'));
+	})
+};
 
-$(document).ready(function(){
+var editServer = function(serverId) {	
+	showLoading();
+	savedServerID = serverId;
+	$.ajax({
+        type: "get",
+        url: mapPathVariablesInUrl(restUri.server.item,{serverId: serverId}),
+        data: { get_param: 'id,name,username,password,connectionUrl,connection.id,prefix,active' },
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json'
+    }).done(function(data){
+		$("#name").val(data.name);
+		$("#username").val(data.username);
+		$("#password").val(data.password);
+		$("#url").val(data.connectionUrl);
+		$("#connectionType").val(data.connectionType.id);
+		$("#prefix").val(data.prefix);
+		$("#active > [value=" + data.active + "]").prop('selected', true);
+		$('#modalServer').modal('show');
+		associatePostServer();
+    }).fail(function(e){
+    	handleError('#global-alert', e);
+    }).always(function(){
+    	hideLoading();
+    });		 
+}
+
+//Delete server
+var associateDeleteServer = function(){
+	$('.deleteServer').confirmation({
+		placement: "left",
+		btnOkLabel: "Delete Server",
+		onConfirm : function(event, element) {
+//			var btn = $(this);
+			var btn = element;
+//			console.log("Deleting Server Id " + btn.data('serverid'));
+			deleteServer(btn.data('serverid'));
+		}
+	});
+};
+
+var deleteServer = function(serverId) {
+	serverId
+	$.ajax({
+        type: "delete",
+        url: mapPathVariablesInUrl(restUri.server.del,{serverId: serverId}),
+        contentType: "application/json; charset=utf-8"
+    }).done(function(data){
+    	var table = $('#tbl_server').DataTable();
+    	table.destroy();
+    	configureDataTable();
+    }).fail(function(e){
+    	handleError('#global-alert', e);
+    }).always(function(){
+    	hideLoading();
+    });	
+}		
+
+//Post server
+var associatePostServer = function(){	
+	var form = $("#newServer");
+	form.validate();
+	$('#Save-server').click(function(e){		
+		if (form.valid()) {
+			$(this).confirmation({
+				placement: "left",
+				btnOkLabel: "Save Server",
+				onConfirm : function(event, element) {			
+					showLoading();
+					var name = $("#name").val();
+					var username = $("#username").val();
+					var password = $("#password").val();
+					var url = $("#url").val();
+					var connectionType = $("#connectionType").val();
+					var prefix = $("#prefix").val();
+					var active = $("#active").val();
+					$.ajax({
+				        type: "post",
+				        url: restUri.server.save,
+				        data: JSON.stringify({id:savedServerID,name:name,username:username,password:password,connectionUrl:url,prefix:prefix,active:active,alias:"AAA","connectionType":{"id":connectionType},"tables":[],"connected":false,"lastError":""}),
+				        contentType: "application/json; charset=utf-8"
+				    }).done(function(data){
+				    	var table = $('#tbl_server').DataTable();
+				    	table.destroy();
+				    	configureDataTable();
+				    	savedServerID = -1;
+				    }).fail(function(e){
+				    	handleError('#global-alert', e);
+				    }).always(function(){
+				    	hideLoading();
+						$('#modalServer').modal('hide');
+				    });
+				}
+			});	
+			$(this).confirmation('show');
+		} else {
+			$(this).confirmation('destroy');
+		}
+		e.preventDefault();
+	});
+};
+
+$(document).ready(function(){	
+
 	configureDataTable();
 	
+	$("#addServer").click(function(){
+		$("#modalServer").find('form').trigger('reset');
+	});
+	
 	// Get Connection types
-	 $.ajax({
+	$.ajax({
         type: "get",
-        url: "http://localhost:8080/gdma2/rest/connection/list",
+        url: restUri.connection.list,
         data: { get_param: 'id,name' },
         contentType: "application/json; charset=utf-8",
         dataType: 'json'
@@ -40,35 +179,8 @@ $(document).ready(function(){
     	handleError('#global-alert', e);
     }).always(function(){
     	hideLoading();
-    });	
-	
-	// Post new server
-	$("#Save-server").click(function(event){
-		showLoading();
-		var name = $("#name").val();
-		var username = $("#username").val();
-		var password = $("#password").val();
-		var url = $("#url").val();
-		var connectionType = $("#connectionType").val();
-		var prefix = $("#prefix").val();
-		var active = $("#active").val();
+    });
 
-	    $.ajax({
-	        type: "post",
-	        url: restUri.server.save,
-	        data: JSON.stringify({name:name,username:username,password:password,connectionUrl:url,prefix:prefix,active:active,alias:"AAA","connectionType":{"id":connectionType},"tables":[],"connected":false,"lastError":""}),
-	        contentType: "application/json; charset=utf-8",
-	        dataType: 'json'
-	    }).done(function(data){
-			$('#modalServer').modal('hide');
-	    }).fail(function(e){
-	    	handleError('#global-alert', e);
-	    }).always(function(){
-	    	hideLoading();
-	    });	
-	    event.preventDefault();
-	});
-	
 	// Get Server list
 	showLoading();
     $.ajax({
