c9dfd5c5621eb51e2d8f9ca657f6afb6adcee4bf
806827 <devnull@localhost>
2017-04-18 08:53:28 +0100

    FrontEnd changes

--- gdma2-web/src/main/resources/public/main/page/columns.js
+++ gdma2-web/src/main/resources/public/main/page/columns.js
@@ -1,18 +1,16 @@
-var tableSessionId = 1;
+var tableSessionId = sessionStorage.getItem("idTable");;
 var configureDataTable = function(){
 	
 	var config={
 	        fixedHeader: true,
-			order: [[ 0, "asc" ]],
 			"lengthMenu": [ [10, 25, 50, 100], [10, 25, 50, 100] ],
 			"columns": [
-			            { "data": "id" },
-			            { "data": "name" }
+			            { "data": "id" }
 			        ]
 	};
 
 	$('#tbl_column').configureDataTable(config, {
-		url: mapPathVariablesInUrl(restUri.column.table, {id: tableSessionId}), 
+		url: mapPathVariablesInUrl(restUri.column.item, {id: tableSessionId}), 
 		complete: function(){
 	//		hideLoading();
 		}
