52e61c6ecd5ee298a1009479542dcfb6ac7dde65
806587 <devnull@localhost>
2017-06-15 15:06:37 +0100

    fixing IEDGDMA-77 - Inconsistent number of pages are displayed within 'show entries' drop down menu

--- gdma2-web/src/main/resources/public/main/page/connectionTypes.js
+++ gdma2-web/src/main/resources/public/main/page/connectionTypes.js
@@ -4,6 +4,7 @@ var configureDataTable = function() {
 
 	var config = {
 		fixedHeader : true,
+		lengthMenu: [ [10, 25, 50, 100], [10, 25, 50, 100] ],
 		order : [ [ 1, "asc" ] ],
 		idSrc : 'id',
 		"columns" : [
