6039dba78c8c2adb709c79b81853eb6605bec674
Farrukh Mirza <farrukh.mirza@clientsolutions.ie>
2018-11-27 15:05:43 +0000

    RDMA-11 Half problem solved. Rest is due to Microsoft Excel

--- gdma2-adaptor/src/main/java/ie/clients/gdma2/util/CsvDownloader.java
+++ gdma2-adaptor/src/main/java/ie/clients/gdma2/util/CsvDownloader.java
@@ -1,6 +1,7 @@
 package ie.clients.gdma2.util;
 
 import java.sql.Timestamp;
+import java.sql.Types;
 import java.util.Date;
 import java.util.List;
 import java.util.StringJoiner;
@@ -13,7 +14,7 @@ import ie.clients.gdma2.domain.Column;
 import ie.clients.gdma2.domain.ui.DataTableDropDown;
 
 /* READ data and include proper dropdown values:
- */   
+ */
 public class CsvDownloader {
 
 	private static final String EOL = "\r\n";
@@ -25,21 +26,21 @@ public class CsvDownloader {
 	private static final Logger logger = LoggerFactory.getLogger(CsvDownloader.class);
 
 	/*
-	 * @Author: Farrukh Mirza
-	 * Following code has been modified by Farrukh
-	 * Reason: 	When downloading a table that is joined to a lookup table
-	 * 			The result set contains lookup store value (foreign key) instead of lookup display value
-	 * 			However, when importing the same CSV, RDMA expects the lookup display value and not the lookup store value
-	 * 			This results in error while UPSERTING the csv
+	 * @Author: Farrukh Mirza Following code has been modified by Farrukh Reason:
+	 * When downloading a table that is joined to a lookup table The result set
+	 * contains lookup store value (foreign key) instead of lookup display value
+	 * However, when importing the same CSV, RDMA expects the lookup display value
+	 * and not the lookup store value This results in error while UPSERTING the csv
 	 * 
-	 * 			The following changes are expected to return the lookup display value instead, hence fixing the process. 
+	 * The following changes are expected to return the lookup display value
+	 * instead, hence fixing the process.
 	 * 
-	 * 		Please also see DataModuleServiceImpl::dataExport()
+	 * Please also see DataModuleServiceImpl::dataExport()
 	 * 
 	 */
 	public static String createCSV(List<Column> columns, List<TableRowDTO> rows) {
 
-		//create header first
+		// create header first
 		StringBuilder sbTemp = new StringBuilder();
 		if (rows != null && rows.size() > 0) {
 			boolean addComma = false;
@@ -49,177 +50,173 @@ public class CsvDownloader {
 				} else {
 					addComma = true;
 				}
-				sbTemp.append(column.getName()); //TODO: Farrukh: Should this be Alias? Depends on Import
+				sbTemp.append(column.getName()); // TODO: Farrukh: Should this be Alias? Depends on Import
 			}
 			sbTemp.append(EOL);
 
-			//create rows
+			// create rows
 			for (TableRowDTO tableRowDTO : rows) {
 				StringJoiner rowDataJoiner = new StringJoiner(COLUMN_VALUE_DELIMITER);
 				logger.info("row number:" + tableRowDTO.getRowNumber());
-				
-				
+
 				for (Column column : columns) {
 					Object v = tableRowDTO.getColumns().get(column.getName());
-					
+
 					logger.info("Processing: " + column.getName() + ": " + v);
-					
+
 					String colValue = v == null ? " " : getValue(column, v);
-					//If the column value contains a comma(s), then surround the value with double quotes so the comma(s) are escaped 
-					//to ensure that the resultant CSV file will not be 'skewed'.
+					// If the column value contains a comma(s), then surround the value with double
+					// quotes so the comma(s) are escaped
+					// to ensure that the resultant CSV file will not be 'skewed'.
 					if (colValue.indexOf(COLUMN_VALUE_DELIMITER) != -1) {
 						StringBuilder columnValueBuilder = new StringBuilder("");
 						columnValueBuilder.append(COLUMN_VALUE_PRE_SUFFIX);
 						columnValueBuilder.append(colValue.replaceAll(COLUMN_VALUE_PRE_SUFFIX, "\"\""));
 						columnValueBuilder.append(COLUMN_VALUE_PRE_SUFFIX);
-						colValue = columnValueBuilder.toString(); 
-					} 
+						colValue = columnValueBuilder.toString();
+					}
 					rowDataJoiner.add(colValue);
 				}
-				
-				
-				
-				
+
 				sbTemp.append(rowDataJoiner);
 				sbTemp.append(EOL);
 			}
-		}  else {
+		} else {
 			sbTemp.append("No records found\r\n");
 		}
 		return sbTemp.toString();
-	}	
+	}
 
 	private static String getValue(Column column, Object value) {
-		if(column.getDatatableEditorFieldOptions() !=null && !column.getDatatableEditorFieldOptions().isEmpty()  /*isDropDown(value)*/){
-			logger.info("Processing dropdown field: " + column.getName() + ": " + column.getDatatableEditorFieldOptions());
+		if (column.getDatatableEditorFieldOptions() != null
+				&& !column.getDatatableEditorFieldOptions().isEmpty() /* isDropDown(value) */) {
+			logger.info(
+					"Processing dropdown field: " + column.getName() + ": " + column.getDatatableEditorFieldOptions());
 			return getDropDownLabelForValue(column.getDatatableEditorFieldOptions(), value.toString());
-		};
+		}
+		;
 		if (value instanceof Date) {
-			return getDateValue(value);
+			return getDateValue(column, value);
 		} else {
 			return value == null ? "" : value.toString();
 		}
 	}
 
-	private static String getDropDownLabelForValue(List<DataTableDropDown> dropDownValues, String value /*value : 101 */){
+	private static String getDropDownLabelForValue(List<DataTableDropDown> dropDownValues,
+			String value /* value : 101 */) {
 		logger.info("getDropDownValue");
-		//if dropdown value not set return ' '
-		if(value == null || StringUtils.isBlank(value)){
+		// if dropdown value not set return ' '
+		if (value == null || StringUtils.isBlank(value)) {
 			return " ";
 		}
 		String valueLocated = null;
 		for (DataTableDropDown ddLists : dropDownValues) {
-			if(value.equalsIgnoreCase(ddLists.getValue())) {
+			if (value.equalsIgnoreCase(ddLists.getValue())) {
 				logger.info("MATCH!");
 				valueLocated = ddLists.getLabel();
 				break;
 			}
-		}//for 
+		} // for
 		logger.info("value:" + valueLocated);
 		return valueLocated;
 	}
 
-	/*
-	public static String createCSV(List<String> headers, List<TableRowDTO> rows) {
+	private static String getDateValue(Column column, Object object) {
+		Date date;
+		String dateReturned = "";
 
-		//create header first
-		StringBuilder sbTemp = new StringBuilder();
-		if (rows != null && rows.size() > 0) {
-			boolean addComma = false;
-			for (String column : headers) {
-				if (addComma) {
-					sbTemp.append(',');
-				} else {
-					addComma = true;
-				}
-				sbTemp.append(column);
+		switch (column.getColumnType()) {
+		case Types.DATE:
+			if (column != null && column.getColumnTypeString().equalsIgnoreCase("YEAR")) {
+				dateReturned = ((Integer) object).toString();
+			} else {
+				date = getDate(object);
+				dateReturned = Formatter.formatDate(date);
 			}
-			sbTemp.append(EOL);
-
-			//create rows
-			for (TableRowDTO tableRowDTO : rows) {
-				StringJoiner rowDataJoiner = new StringJoiner(COLUMN_VALUE_DELIMITER);
-				logger.info("row number:" + tableRowDTO.getRowNumber());
-				tableRowDTO.getColumns().forEach((k,v)->{
-					logger.info(k);
-					String colValue = v == null ? " " : getValue(v);
-					//If the column value contains a comma(s), then surround the value with double quotes so the comma(s) are escaped 
-					//to ensure that the resultant CSV file will not be 'skewed'.
-					if (colValue.indexOf(COLUMN_VALUE_DELIMITER) != -1) {
-						StringBuilder columnValueBuilder = new StringBuilder("");
-						columnValueBuilder.append(COLUMN_VALUE_PRE_SUFFIX);
-						columnValueBuilder.append(colValue.replaceAll(COLUMN_VALUE_PRE_SUFFIX, "\"\""));
-						columnValueBuilder.append(COLUMN_VALUE_PRE_SUFFIX);
-						colValue = columnValueBuilder.toString(); 
-					} 
-					rowDataJoiner.add(colValue);
-				});
-				sbTemp.append(rowDataJoiner);
-				sbTemp.append(EOL);
-			}
-		}  else {
-			sbTemp.append("No records found\r\n");
+			break;
+		case Types.TIMESTAMP:
+			date = getDate(object);
+			dateReturned = Formatter.formatDateTime(date);
+			break;
+		case Types.TIME:
+			date = getDate(object);
+			dateReturned = Formatter.formatTime(date);
+			break;
+		default:
+			date = getDate(object);
+			dateReturned = Formatter.formatDate(date);
+			break;
 		}
-		return sbTemp.toString();
-	}	
 
-	private static String getValue(Object object) {
-		if(isDropDown(object)){
-			return getDropDownValue((DropDownColumn)object);
-		};
-		if (object instanceof Date) {
-			return getDateValue(object);
-		} else {
-			return object == null ? "" : object.toString();
-		}
+		return dateReturned;
 	}
 
-	private static boolean isDropDown(Object object){
-		logger.info("isDropDown: " + (object instanceof ie.clients.gdma2.domain.ui.DropDownColumn));
-		return (object instanceof ie.clients.gdma2.domain.ui.DropDownColumn);
-	}
-	
-	private static String getDropDownValue(DropDownColumn dropDown){
-		logger.info("getDropDownValue");
-		String value = dropDown.getValue(); //value : 101 
-		//if dropdown value not set return ' '
-		if(value == null || StringUtils.isBlank(value)){
-			return " ";
-		}
-		String valueLocated = null;
-		boolean isLocated = false;
-		for (Object ddLists : dropDown.getDropdownOptions()) {
-			List ddList = (List) ddLists;
-			if (!isLocated){
-				for (int i = 0; i < ddList.size(); i++) {
-					Object rowNumber = ddList.get(0);
-					Object store = ddList.get(1);
-					Object val = ddList.get(2);
-
-					if(value.equalsIgnoreCase(store.toString())){
-						logger.info("MATCH!");
-						valueLocated = val.toString();
-						isLocated = true;
-					}
-				}//for inner
-			}
-
-		}//for outter
-		logger.info("value:" + valueLocated);
-		return valueLocated;
-	}
-	*/
-
-	private static String getDateValue(Object object) {
+	private static Date getDate(Object object) {
 		Date date;
 		if (object instanceof Timestamp) {
-			return object.toString();
+			date = new Date(((Timestamp) object).getTime());
 		} else if (object instanceof java.sql.Date) {
 			date = new Date(((java.sql.Date) object).getTime());
 		} else {
 			date = (Date) object;
 		}
-		return Formatter.formatDate(date);
+
+		return date;
 	}
+
+	/*
+	 * public static String createCSV(List<String> headers, List<TableRowDTO> rows)
+	 * {
+	 * 
+	 * //create header first StringBuilder sbTemp = new StringBuilder(); if (rows !=
+	 * null && rows.size() > 0) { boolean addComma = false; for (String column :
+	 * headers) { if (addComma) { sbTemp.append(','); } else { addComma = true; }
+	 * sbTemp.append(column); } sbTemp.append(EOL);
+	 * 
+	 * //create rows for (TableRowDTO tableRowDTO : rows) { StringJoiner
+	 * rowDataJoiner = new StringJoiner(COLUMN_VALUE_DELIMITER);
+	 * logger.info("row number:" + tableRowDTO.getRowNumber());
+	 * tableRowDTO.getColumns().forEach((k,v)->{ logger.info(k); String colValue = v
+	 * == null ? " " : getValue(v); //If the column value contains a comma(s), then
+	 * surround the value with double quotes so the comma(s) are escaped //to ensure
+	 * that the resultant CSV file will not be 'skewed'. if
+	 * (colValue.indexOf(COLUMN_VALUE_DELIMITER) != -1) { StringBuilder
+	 * columnValueBuilder = new StringBuilder("");
+	 * columnValueBuilder.append(COLUMN_VALUE_PRE_SUFFIX);
+	 * columnValueBuilder.append(colValue.replaceAll(COLUMN_VALUE_PRE_SUFFIX,
+	 * "\"\"")); columnValueBuilder.append(COLUMN_VALUE_PRE_SUFFIX); colValue =
+	 * columnValueBuilder.toString(); } rowDataJoiner.add(colValue); });
+	 * sbTemp.append(rowDataJoiner); sbTemp.append(EOL); } } else {
+	 * sbTemp.append("No records found\r\n"); } return sbTemp.toString(); }
+	 * 
+	 * private static String getValue(Object object) { if(isDropDown(object)){
+	 * return getDropDownValue((DropDownColumn)object); }; if (object instanceof
+	 * Date) { return getDateValue(object); } else { return object == null ? "" :
+	 * object.toString(); } }
+	 * 
+	 * private static boolean isDropDown(Object object){ logger.info("isDropDown: "
+	 * + (object instanceof ie.clients.gdma2.domain.ui.DropDownColumn)); return
+	 * (object instanceof ie.clients.gdma2.domain.ui.DropDownColumn); }
+	 * 
+	 * private static String getDropDownValue(DropDownColumn dropDown){
+	 * logger.info("getDropDownValue"); String value = dropDown.getValue(); //value
+	 * : 101 //if dropdown value not set return ' ' if(value == null ||
+	 * StringUtils.isBlank(value)){ return " "; } String valueLocated = null;
+	 * boolean isLocated = false; for (Object ddLists :
+	 * dropDown.getDropdownOptions()) { List ddList = (List) ddLists; if
+	 * (!isLocated){ for (int i = 0; i < ddList.size(); i++) { Object rowNumber =
+	 * ddList.get(0); Object store = ddList.get(1); Object val = ddList.get(2);
+	 * 
+	 * if(value.equalsIgnoreCase(store.toString())){ logger.info("MATCH!");
+	 * valueLocated = val.toString(); isLocated = true; } }//for inner }
+	 * 
+	 * }//for outter logger.info("value:" + valueLocated); return valueLocated; }
+	 * 
+	 * private static String getDateValue(Object object) { Date date; if (object
+	 * instanceof Timestamp) { return object.toString(); } else if (object
+	 * instanceof java.sql.Date) { date = new Date(((java.sql.Date)
+	 * object).getTime()); } else { date = (Date) object; } return
+	 * Formatter.formatDate(date); }
+	 */
 
 }
--- gdma2-adaptor/src/main/java/ie/clients/gdma2/util/Formatter.java
+++ gdma2-adaptor/src/main/java/ie/clients/gdma2/util/Formatter.java
@@ -10,22 +10,21 @@ import java.sql.Timestamp;
 /**
  * @author RGILL
  * 
- *         To change the template for this generated type comment go to Window - Preferences - Java - Code Generation -
- *         Code and Comments
+ *         To change the template for this generated type comment go to Window -
+ *         Preferences - Java - Code Generation - Code and Comments
  */
 public class Formatter {
 
-	//java.sql.Date = 91
+	// java.sql.Date = 91
 	public static String dateFormat = "yyyy-MM-dd";
 	public static String dateFormat2 = "dd/MM/yyyy";
 
-
-	//java.sql.Time = 92
+	// java.sql.Time = 92
 	public static String timeFormat = "HH:mm:ss";
 
-	//java.sql.Timestamp = 93
+	// java.sql.Timestamp = 93
 	// (at some DB's also called DATETIME),
-	public static String timeStampFormat = "yyyy-MM-dd HH:mm:ss"; 
+	public static String timeStampFormat = "yyyy-MM-dd HH:mm:ss";
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
 
@@ -34,7 +33,7 @@ public class Formatter {
 	private static SimpleDateFormat sdf3 = new SimpleDateFormat(timeFormat);
 
 	private static SimpleDateFormat sdf4 = new SimpleDateFormat(timeStampFormat);
-	
+
 	private static String frontEndTimeStampFormat = "dd-MMM-yyyy HH:mm:ss.SSS";
 
 	public static String escapeQuotes(String in) {
@@ -64,6 +63,20 @@ public class Formatter {
 			return sdf.format(date);
 	}
 
+	public static String formatTime(Date date) {
+		if (date == null)
+			return "";
+		else
+			return sdf3.format(date);
+	}
+
+	public static String formatDateTime(Date date) {
+		if (date == null)
+			return "";
+		else
+			return sdf4.format(date);
+	}
+
 	// Try 2 ways of parsing
 	// First - treat it as a long
 	// Second - treat it as a string
@@ -89,7 +102,6 @@ public class Formatter {
 		return date;
 	}
 
-
 	public static Date parseDateTime(String value) throws Exception {
 		Date date = null;
 		try {
@@ -102,12 +114,12 @@ public class Formatter {
 				date = sdf4.parse(value);
 			} catch (Exception e) {
 				date = null;
-				throw new Exception("Could not pase value [" + value + "] into a datetime");
+				throw new Exception("Could not parse value [" + value + "] into a datetime");
 			}
 		}
 		return date;
 	}
-	
+
 	public static Timestamp parseTimeStamp(String value) throws Exception {
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(frontEndTimeStampFormat);
 		try {
@@ -118,7 +130,7 @@ public class Formatter {
 			throw new Exception("Could not pase value [" + value + "] into a TimeStamp");
 		}
 	}
-	
+
 	public static String getDateStringFromTimestamp(String timestamp) throws ParseException {
 		String value = "";
 
